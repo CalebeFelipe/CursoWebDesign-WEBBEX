d6d04e04fcc5060e4b2e91486a053d1ddaec7898
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-24 10:27:06 -0300

    feat: adicionando boas práticas no exercicio 11 de JS

--- exercicios_JavaScript/exercicio11_JS/js/script.js
+++ exercicios_JavaScript/exercicio11_JS/js/script.js
@@ -1,15 +1,17 @@
-const output = document.querySelector('#output')
+'use strict'
+
+const output = document.querySelector('#output');
 let dados = [
     {'nome': 'Calebe', 'email': 'ca@server.com'},
     {'nome': 'Mylena', 'email': 'my@server.com'},
     {'nome': 'Agelma', 'email': 'ag@server.com'},
-]
-let msg = ''
+];
+let msg = '';
 
 
 let dado = dados.map(dado => {
-    msg += `<p>nome: ${dado.nome}</p>`
-    msg += `<p>email: ${dado.email}</p><br>`
-})
+    msg += `<p>nome: ${dado.nome}</p>`;
+    msg += `<p>email: ${dado.email}</p><br>`;
+});
 
-output.innerHTML = msg
+output.innerHTML = msg;
