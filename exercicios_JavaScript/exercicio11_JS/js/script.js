032ccb08f1120e1e1fac2cd9f0a37273eaffcc87
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-24 09:15:27 -0300

    feat: adicionando exercicio 11 de JS

---
+++ exercicios_JavaScript/exercicio11_JS/js/script.js
@@ -0,0 +1,15 @@
+const output = document.querySelector('#output')
+let dados = [
+    {'nome': 'Calebe', 'email': 'ca@server.com'},
+    {'nome': 'Mylena', 'email': 'my@server.com'},
+    {'nome': 'Agelma', 'email': 'ag@server.com'},
+]
+let msg = ''
+
+
+let dado = dados.map(dado => {
+    msg += `<p>nome: ${dado.nome}</p>`
+    msg += `<p>email: ${dado.email}</p><br>`
+})
+
+output.innerHTML = msg
