48e1ece24dbe3b04712bd66356317780407cd52d
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-25 10:17:57 -0300

    feat: adicionando exercicio 15 de JS

---
+++ exercicios_JavaScript/exercicio 15_JS/js/script.js
@@ -0,0 +1,7 @@
+(function(){
+    let nome = 'Calebe Felipe';
+    let sobrenome = 'Alves Freitas';
+    let campoNome = document.querySelector('#campoNome');
+
+    campoNome.value = `${nome} ${sobrenome}`
+})()
