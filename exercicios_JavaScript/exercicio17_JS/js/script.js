0a717416e6d312bd160e66696f2bd7cc842646bb
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-26 09:13:23 -0300

    feat: adicionando exercicio 17 de JS

---
+++ exercicios_JavaScript/exercicio17_JS/js/script.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+    let paragrafos = document.querySelectorAll('.paragrafo');
+
+    for (let i = 0; i < paragrafos.length; i++) {
+        paragrafos[i].textContent = 'Calebe'
+    }
+
+})()
