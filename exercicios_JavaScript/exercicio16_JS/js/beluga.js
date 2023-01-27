ff66ec81f69dd5b0d15e84f979e9ade001d46c6f
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-27 10:39:07 -0300

    feat: atualizando exercicio 16 de JS com nova função

--- exercicios_JavaScript/exercicio16_JS/js/beluga.js
+++ exercicios_JavaScript/exercicio16_JS/js/beluga.js
@@ -1,23 +1,31 @@
 export function calcularMedia(notas){
-let quantidadeDeNotas = notas.length
-let somaDasNotas = notas.reduce((acc, currentValue) => acc += currentValue, 0)
-let total = somaDasNotas / quantidadeDeNotas
-return total.toFixed(2)
+    let quantidadeDeNotas = notas.length
+    let somaDasNotas = notas.reduce((acc, currentValue) => acc += currentValue, 0)
+    let total = somaDasNotas / quantidadeDeNotas
+    return total.toFixed(2)
 }
 
 export function recuperarNumerosDeColunas(tds){
-let arr = [];
-for(let i = 0; i < tds.length; i++) {
-if(!isNaN(tds[i].textContent) && tds[i].textContent !== '') {
-arr.push(parseFloat(tds[i].textContent))
-}
-};
-return arr;
+    let arr = [];
+    for(let i = 0; i < tds.length; i++) {
+        if(!isNaN(tds[i].textContent) && tds[i].textContent !== '') {
+            arr.push(parseFloat(tds[i].textContent))
+        }
+    };
+    return arr;
 }
 
 export const sortear = (n = 0) => {
-let numeroSorteado = Math.random()
-numeroSorteado = Math.floor(numeroSorteado*n)
+    let numeroSorteado = Math.random()
+    numeroSorteado = Math.floor(numeroSorteado*n)
 
-return numeroSorteado
+    return numeroSorteado
+}
+
+export function aprovadoOuReprovado(tds) {
+    if(tds.some((item) => item == 0)) {
+        return 'reprovado'
+    }
+
+    return 'aprovado'
 }
--- exercicios_JavaScript/exercicio16_JS/js/index.js
+++ exercicios_JavaScript/exercicio16_JS/js/index.js
@@ -1,15 +1,17 @@
-import {recuperarNumerosDeColunas, calcularMedia} from './beluga.js'
+import {recuperarNumerosDeColunas, calcularMedia, aprovadoOuReprovado} from './beluga.js'
 
 let trs = document.querySelectorAll('.js-tabela tbody .aluno');
 
 for(let i = 0; i < trs.length; i++) {
-let linhaAluno = trs[i];
-let tds = linhaAluno.querySelectorAll('td');
+    let linhaAluno = trs[i];
+    let tds = linhaAluno.querySelectorAll('td');
 
-let notas = recuperarNumerosDeColunas(tds);
+    let notas = recuperarNumerosDeColunas(tds);
+    console.log(notas)
+    let media = calcularMedia(notas)
+    let resultadoFinal = aprovadoOuReprovado(notas)
 
-let media = calcularMedia(notas)
-
-linhaAluno.querySelector('.aluno-m').textContent = media;
+    linhaAluno.querySelector('.aluno-m').textContent = media;
+    linhaAluno.querySelector('.aluno-aprov').textContent = resultadoFinal
 
 };
