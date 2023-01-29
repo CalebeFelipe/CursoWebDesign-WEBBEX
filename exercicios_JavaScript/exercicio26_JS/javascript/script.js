2eaf37c5ed75ef3916330b296fc313f6543db04d
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-29 20:23:26 -0300

    feat: adicionando melhoria progressiva no exercicio 26 de JS

--- exercicios_JavaScript/exercicio26_JS/javascript/script.js
+++ exercicios_JavaScript/exercicio26_JS/javascript/script.js
@@ -1,6 +1,9 @@
 let root = document.querySelector('html');
 let menuHamburguer = document.querySelector('.header-nav__hamburgger');
 
+root.classList.remove('no-js');
+root.classList.add('js');
+
 function abrirMenu(){
     root.classList.toggle('menu-opened');
 }
