7a17dbf6948d0a4ff2b7611d4d3a69acf3a31be1
Calebe Felipe Alves Freitas <fcalebe7@gmail.com>
2023-01-30 08:40:14 -0300

    feat: adicionando mais acessibilidade exercicio 26 de JS

--- exercicios_JavaScript/exercicio26_JS/javascript/script.js
+++ exercicios_JavaScript/exercicio26_JS/javascript/script.js
@@ -1,11 +1,21 @@
-let root = document.querySelector('html');
+let html = document.querySelector('html');
 let menuHamburguer = document.querySelector('.header-nav__hamburgger');
+let ariaControl = menuHamburguer.getAttribute('aria-controls');
 
-root.classList.remove('no-js');
-root.classList.add('js');
+let menu = document.getElementById(ariaControl);
+
+console.log(ariaControl)
+
+html.classList.remove('no-js');
+html.classList.add('js');
 
 function abrirMenu(){
-    root.classList.toggle('menu-opened');
+    html.classList.toggle('menu-opened');
+
+    let ariaExpanded = this.getAttribute('aria-expanded') === 'true';
+
+    menuHamburguer.setAttribute('aria-expanded', !ariaExpanded);
+    menu.setAttribute('aria-expanded', !ariaExpanded);
 }
 
 menuHamburguer
